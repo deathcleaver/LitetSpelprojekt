724f007b33c2d5d95257185637e932070a127248
deathcleaver <chaos2night@gmail.com>
2015-04-11 12:04:36 +0200

    Added opengl debug callback

--- Spelprojekt/Spelprojekt/src/game/game.cpp
+++ Spelprojekt/Spelprojekt/src/game/game.cpp
@@ -3,6 +3,66 @@
 //#include <iostream>
 //#include <Windows.h>
 
+// opengl debug callback
+
+#ifdef _DEBUG
+extern "C"
+{
+	void APIENTRY openglCallbackFunction(GLenum source,
+		GLenum type,
+		GLuint id,
+		GLenum severity,
+		GLsizei length,
+		const GLchar* message,
+		void* userParam)
+	{
+
+		std::cout << "---------------------opengl-callback-start------------" << std::endl;
+		std::cout << "message: " << message << std::endl;
+		std::cout << "type: ";
+		switch (type) {
+		case GL_DEBUG_TYPE_ERROR:
+			std::cout << "ERROR";
+			break;
+		case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+			std::cout << "DEPRECATED_BEHAVIOR";
+			break;
+		case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+			std::cout << "UNDEFINED_BEHAVIOR";
+			break;
+		case GL_DEBUG_TYPE_PORTABILITY:
+			std::cout << "PORTABILITY";
+			break;
+		case GL_DEBUG_TYPE_PERFORMANCE:
+			std::cout << "PERFORMANCE";
+			break;
+		case GL_DEBUG_TYPE_OTHER:
+			std::cout << "OTHER";
+			break;
+		}
+		std::cout << std::endl;
+
+		std::cout << "id: " << id << std::endl;
+		std::cout << "severity: ";
+		switch (severity){
+		case GL_DEBUG_SEVERITY_LOW:
+			std::cout << "LOW";
+			break;
+		case GL_DEBUG_SEVERITY_MEDIUM:
+			std::cout << "MEDIUM";
+			break;
+		case GL_DEBUG_SEVERITY_HIGH:
+			std::cout << "HIGH";
+			break;
+		}
+		std::cout << std::endl;
+		std::cout << "---------------------opengl-callback-end--------------" << std::endl;
+	}
+
+}
+#endif
+
+
 Game::~Game()
 {
 	if (engine)
@@ -19,6 +79,29 @@ Game::~Game()
 
 void Game::init(GLFWwindow* windowRef)
 {
+	// register callback
+#ifdef _DEBUG
+	if (glDebugMessageCallback){
+		std::cout << "Register OpenGL debug callback " << std::endl;
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback((GLDEBUGPROC)openglCallbackFunction, nullptr);
+		GLuint unusedIds = 0;
+		glDebugMessageControl(GL_DONT_CARE,
+			GL_DONT_CARE,
+			GL_DONT_CARE,
+			0,
+			&unusedIds,
+			true);
+	}
+	else
+	{
+		std::cout << "glDebugMessageCallback not available" << std::endl;
+	}
+#endif
+
+
+
+
 	engine = new Engine();
 	engine->init();
 	content = new ContentManager();
@@ -36,6 +119,7 @@ void Game::init(GLFWwindow* windowRef)
 
 void Game::mainLoop()
 {
+
 	clock_t start = clock();
 	//how much of a second have passed since last frame
 	float deltaTime = 0.0f;
