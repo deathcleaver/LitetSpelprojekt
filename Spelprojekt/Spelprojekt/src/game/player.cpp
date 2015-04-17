4e8436d6861bed93c8f1a2f708564d6589880a7a
Carcity <carcity.h@gmail.com>
2015-04-17 15:13:08 +0200

    New commit

--- Spelprojekt/Spelprojekt/src/game/player.cpp
+++ Spelprojekt/Spelprojekt/src/game/player.cpp
@@ -10,7 +10,7 @@ void Player::init()
 int Player::update(float deltaTime)
 {
 	timepass += deltaTime;
-	rotate(0, deltaTime, 0);
+	//rotate(0, deltaTime, 0);
 	moveTo(1 * 13, 1 * 5, 0);
 	return 0;
 }
