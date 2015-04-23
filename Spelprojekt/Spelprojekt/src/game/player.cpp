4758ffe93ff0afeaba6d154d59785b1c6e21afd9
Kraetyz <krae_tyz@hotmail.com>
2015-04-23 21:04:59 +0200

    Fixade en krasch

--- Spelprojekt/Spelprojekt/src/game/player.cpp
+++ Spelprojekt/Spelprojekt/src/game/player.cpp
@@ -332,13 +332,16 @@ int Player::update(UserInput* userInput, Map* map, Audio* audio, float deltaTime
 		}
 		if (currentRune == 0)
 		{
-			currentRune = currentSpawn->getRune();
-			if (currentRune == FLAME)
-				attackRect.initGameObjectRect(&weaponMatrix, 0.8, 2.5);
-			else if (currentRune == SPARK)
-				DMG += 1;
-			else if (currentRune == FORCE)
-				shield = 2;
+			if (currentSpawn)
+			{
+				currentRune = currentSpawn->getRune();
+				if (currentRune == FLAME)
+					attackRect.initGameObjectRect(&weaponMatrix, 0.8, 2.5);
+				else if (currentRune == SPARK)
+					DMG += 1;
+				else if (currentRune == FORCE)
+					shield = 2;
+			}
 			audio->playSound(0);//item
 		}
 	}
