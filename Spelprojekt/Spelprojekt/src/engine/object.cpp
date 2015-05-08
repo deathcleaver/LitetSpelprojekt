023634106b3c0396ab9cff959b61ee91eae68492
ResQQ <resq@live.se>
2015-05-08 17:05:06 +0200

    asd

--- Spelprojekt/Spelprojekt/src/engine/object.cpp
+++ Spelprojekt/Spelprojekt/src/engine/object.cpp
@@ -308,6 +308,7 @@ bool Object::loadPNG(std::string imagepath)
 	//
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	return false;
 }
 
 int Object::getFaces()
