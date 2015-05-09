efe3d7bf0c4fedb270aa171bc9376de2fee2f6cc
ResQQ <resq@live.se>
2015-05-09 13:30:58 +0200

    Mansion :house:

--- Spelprojekt/Spelprojekt/src/engine/GBuffer.cpp
+++ Spelprojekt/Spelprojekt/src/engine/GBuffer.cpp
@@ -266,9 +266,12 @@ void Gbuffer::render(glm::vec3* campos, const GUI* gui, const Map* map, const Co
 	glBindBuffer(GL_ARRAY_BUFFER, renderQuad);
 	glBindVertexArray(renderVao);
 
+	//campos
 	glProgramUniform3f(*shaderPtr, uniformCamPos, campos->x, campos->y, 1.0f);
-	//glProgramUniform3f(*shaderPtr, uniformPlayerPos, campos->x, campos->y, 1.0f);
-	glProgramUniform3f(*shaderPtr, uniformPlayerPos, playerPos[0], playerPos[1], 1.0f); //<--- DETTA MÅSTE FIXAS
+	
+	//playerpos
+	glProgramUniform3f(*shaderPtr, uniformPlayerPos, campos->x, campos->y, 1.0f);
+	//glProgramUniform3f(*shaderPtr, uniformPlayerPos, playerPos[0], playerPos[1], 1.0f); //<--- DETTA MÅSTE FIXAS
 	
 	glProgramUniform1ui(*shaderPtr, uniformNrLightPos, nrLight);
 
